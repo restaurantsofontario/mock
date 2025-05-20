ada9357ad6cc8893a4d97fc56da34a963caa4a1e
Rakeen Huq <r2huq@uwaterloo.ca>
2025-05-20 14:54:45 -0400

    added home section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-import './App.css'
+import { useState } from 'react'
+import Home from './Home/Home'
+import Header from './Header/Header';
 
 function App() {
+  const [activeTab, setActiveTab] = useState('home')
+  setActiveTab;
+
   return (
     <>
-      <div>Hello World!</div>
+      <div className='
+        min-h-screen
+        flex
+        justify-center
+        bg-[url(/stock-background.jpg)]
+        bg-fixed
+      '>
+        <div className='
+          w-full
+          max-w-[1024px]
+          flex
+          flex-col
+        '>
+          <Header/>
+
+          {/* tabs */}
+          {activeTab === 'home' ? <Home/> : ''}
+        </div>
+      </div>
     </>
   )
 }
